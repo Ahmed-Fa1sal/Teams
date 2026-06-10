36046f539a781227d6b16c4886a45fbbf63ba3f7
Mahmoud Saeed <frezeqb@gmail.com>
2026-06-10 09:54:36 +0300

    calling cors

--- src/main/java/com/teams/teams/config/SecurityConfig.java
+++ src/main/java/com/teams/teams/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200","https://192.168.100.87:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
